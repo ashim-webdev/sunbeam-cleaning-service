4b2f8d8aed17632bf2bd92f27661c69d94ceb635
ashim-webdev <ashimdev5035@gmail.com>
2026-07-02 16:46:20 +0100

    frontend http changes

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,12 +1,8 @@
 import { io } from "socket.io-client";
 
-const URL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:8800"
-    : "https://your-backend-url.com";
+const URL = import.meta.env.VITE_APP_BASE_URL;
 
 export const socket = io(URL, {
   withCredentials: true,
   transports: ["websocket"],
-  // autoConnect: true,
 });
