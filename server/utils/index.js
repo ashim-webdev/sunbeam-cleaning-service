8f1b21c32472b5ecfa3ca3d1ba41cf95bf8fa77a
ashim-webdev <ashimdev5035@gmail.com>
2026-07-02 17:47:56 +0100

    sameSite fix

--- server/utils/index.js
+++ server/utils/index.js
@@ -7,8 +7,8 @@ const createJWT = (res, userId) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
-    sameSite: "strict", // Prevent CSRF attacks {change to (strict)}
+    secure: process.env.NODE_ENV !== "production", // Use secure cookies in production
+    sameSite: "none", // Prevent CSRF attacks {change to (strict)}
     maxAge: 1 * 24 * 60 * 60 * 1000, // 1 days
   });
 };
